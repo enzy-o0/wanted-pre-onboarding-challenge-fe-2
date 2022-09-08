d6d32dec743b9c81b61cf26fdcf1ae5deb645235
“enzy-o0” <oej3659@daum.net>
2022-09-08 10:59:10 +0900

    feat: 기능 추가

--- index.js
+++ index.js
@@ -4,28 +4,64 @@
  * @property {string} content
  * @property {boolean} isCompleted
  * @property {string} category
- * @property {Array=} tags
+ * @property {Array<string>=} tags
  */
 
 /** @function createTodo
  * @param {string} id
  * @param {string} content
+ * @param {string} category
+ * @param {Array<string>=} tags
  */
-const createTodo = (id, content) => {};
+
+let todoArr = [Item];
+const createTodo = (id, content, category, tags) => {
+  const item = {
+    id: id,
+    content: content,
+    isCompleted: false,
+    category: category,
+    tags: tags,
+  };
+
+  return todoArr.push(item);
+};
 
 /** @function readTodo
  * @param {string} id
  * @returns {Item}
  */
-const readTodo = (id) => {};
+const readTodo = (id) => {
+  return todoArr.find((todo) => todo.id === id);
+};
+
+/** @function readAllTodo
+ * @returns {Item}
+ */
+const readAllTodo = () => {
+  return todoArr;
+};
 
 /** @function updateTodo
  * @param {string} id
  * @param {Item=} item
  */
-const updateTodo = (id, item) => {};
+const updateTodo = (id, content, isCompleted, category, tags) => {
+  const todo = {
+    content: content,
+    isCompleted: isCompleted,
+    category: category,
+    tags: tags,
+  };
+
+  let idx = todoArr.findIndex((todo) => todo.id === id);
+  return (todoArr[idx] = todo);
+};
 
 /** @function deleteTodo
  * @param {string} id
  */
-const deleteTodo = (id) => {};
+const deleteTodo = (id) => {
+  let idx = todoArr.findIndex((todo) => todo.id === id);
+  return delete todoArr[idx];
+};
